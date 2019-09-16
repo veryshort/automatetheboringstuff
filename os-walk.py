0ce6d8d0b935c42a3f0b4a25d864a4a55eb1fb2b
bj <choibongjoon@gmail.com>
2019-09-16 20:36:59 +0900

    renameDates ...

---
+++ os-walk.py
@@ -0,0 +1,13 @@
+# p202
+import os
+
+for folderName, subfolders, filenames in os.walk('./delicious'):
+    print('The current folder is ' + folderName)
+
+    for subfolder in subfolders:
+        print('SUBFOLDER OF ' + folderName + ': ' + subfolder)
+
+    for filename in filenames:
+        print('FILE INSIDE ' + folderName + ': ' + filename)
+
+    print('')
---
+++ renameDates.py
@@ -0,0 +1,8 @@
+import shutil, os, re
+
+datePatter = re.compile(r"""&(.*?)
+    ((0|1)?\d)-
+    ((0|1|2|3)?\d)-
+    ((19|20)\d\d)
+    (.*?)$
+    """, re.VERBOSE)
