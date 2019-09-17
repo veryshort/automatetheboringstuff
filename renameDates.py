55846729aa1def351448675fa7b1523cd02b1ea5
bj <choibongjoon@gmail.com>
2019-09-18 00:17:44 +0900

    use paren instead of line continuation

--- renameDates.py
+++ renameDates.py
@@ -1,8 +1,35 @@
 import shutil, os, re
 
-datePatter = re.compile(r"""&(.*?)
+datePattern = re.compile(r"""^(.*?)
     ((0|1)?\d)-
     ((0|1|2|3)?\d)-
     ((19|20)\d\d)
     (.*?)$
     """, re.VERBOSE)
+#
+for amerFilename in os.listdir('.'):
+    mo = datePattern.search(amerFilename)
+
+    #
+    if mo == None:
+        continue
+
+    #
+    beforePart = mo.group(1)
+    monthPart  = mo.group(2)
+    dayPart    = mo.group(4)
+    yearPart   = mo.group(6)
+    afterPart  = mo.group(8)
+
+    #
+    euroFilename = ( beforePart + dayPart + '-' + monthPart
+            + '-' + yearPart + afterPart )
+
+    #
+    absWorkingDir = os.path.abspath('.')
+    amerFilename = os.path.join(absWorkingDir, amerFilename)
+    euroFilename = os.path.join(absWorkingDir, euroFilename)
+
+    #
+    print('Renaming "%s" to "%s"...' % (amerFilename, euroFilename))
+    #shutil.move(amerFilename, euroFilename)
